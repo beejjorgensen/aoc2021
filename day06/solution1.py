d43c6851bc6800a2b8eeb8a64750f5d3ccd8a1b5
Brian 'Beej Jorgensen' Hall <beej@beej.us>
2021-12-06 20:22:37 -0800

    implemented

--- day06/solution1.py
+++ day06/solution1.py
@@ -1,10 +1,16 @@
-day_counts = [0] * 8
+day_counts = [0] * 9
 
-fp =open("input1.txt")
+fp =open("input.txt")
 line = fp.read()
 fp.close()
 
 for v in line.strip().split(","):
     day_counts[int(v)] += 1
 
+for _ in range(256):
+    ready = day_counts.pop(0)
+    day_counts.append(ready)
+    day_counts[6] += ready
+
+print(sum(day_counts))
 
