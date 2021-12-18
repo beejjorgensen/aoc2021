ec43593fe49e5231d055855a9e179b28585de2d7
Brian "Beej Jorgensen" Hall <beej@beej.us>
2021-12-17 16:28:44 -0800

    remove cruft

--- day16/solution2.py
+++ day16/solution2.py
@@ -29,26 +29,6 @@ operator = {
 }
 
 def read_data(sample=None):
-    sample_map = {
-        1: "8A004A801A8002F478",             # 16
-        2: "620080001611562C8802118E34",     # 12
-        3: "C0015000016115A2E0802F182340",   # 23
-        4: "A0016C880162017C3686B18A3D4780", # 31
-
-        'A': "d2fe28",                       # Literal 2021
-        'B': "38006f45291200",               # Op and literal 10 then 20, length
-        'C': "EE00D40C823060",               # Op and 3 literals, count
-
-        'D': "C200B40A82", # finds the sum of 1 and 2, resulting in the value 3.
-        'E': "04005AC33890", # finds the product of 6 and 9, resulting in the value 54.
-        'F': "880086C3E88112", # finds the minimum of 7, 8, and 9, resulting in the value 7.
-        'G': "CE00C43D881120", # finds the maximum of 7, 8, and 9, resulting in the value 9.
-        'H': "D8005AC2A8F0", # produces 1, because 5 is less than 15.
-        'I': "F600BC2D8F", # produces 0, because 5 is not greater than 15.
-        'J': "9C005AC2F8F0", # produces 0, because 5 is not equal to 15.
-        'K': "9C0141080250320F1802104A08", # produces 1, because 1 + 3 = 2 * 2.
-    }
-
     global data
 
     if sample is None:
@@ -72,9 +52,6 @@ def value_of(data, offset=0):
 
     def ds(a, ln=1):
         b = a + ln
-        #print()
-        #print(a, ln, b)
-        #print(offset, a+offset, b+offset)
         return int(data[offset+a:offset+b], 2)
     
     version = ds(0, 3)
@@ -96,16 +73,6 @@ def value_of(data, offset=0):
 
         return total, packet_len 
         
-    # return operator on all subpackets, recursively
-    def compute_total(v):
-        nonlocal total
-
-        if total is None:
-            total = v
-
-        else:
-            total += v  # TODO unhardcode operator
-
     done = False
 
     length_type = ds(6)
@@ -139,8 +106,7 @@ def value_of(data, offset=0):
     return result, packet_len
 
 data = read_data()
-#print(data)
 
 t, _ = value_of(data)
 
-print(t, _)
+print(t)
