87dc4de8ffbc9c44b462bbc88a9f131df59ad773
Brian "Beej Jorgensen" Hall <beej@beej.us>
2021-12-20 09:02:49 -0800

    more exploder

--- day18/solution1.py
+++ day18/solution1.py
@@ -1,30 +1,39 @@
-def preorder(n, depth=0):
-	if isinstance(n, int):
-		print(f"{depth}: leaf: {n}")
-		return n
-	
-	print(f">{depth}: {n}")
+def preorder(n, depth=0, exploder=None):
+    if isinstance(n, int):
+        print(f"{depth}: leaf: {n}")
+        return n, exploder
+    
+    print(f">{depth}: {n}")
 
-	left = preorder(n[0], depth+1)
-	right = preorder(n[1], depth+1)
+    if depth == 4 and exploder is None:
+        assert(isinstance(n[0], int) and isinstance(n[1], int))
+        print(f"Explode {n}")
+        exploder = n
+        return 0, exploder
 
-	return [left, right]
+    left, exploder = preorder(n[0], depth+1, exploder)
+    right, exploder = preorder(n[1], depth+1, exploder)
 
-	print(f"<{depth}: {n}")
+    print(f"<{depth}: {n}")
+
+    return [left, right], exploder
+
+
 """
 inputs = (
-	[1,2],
-	[[1,2],3],
-	[9,[8,7]],
-	[[1,9],[8,5]],
+    [1,2],
+    [[1,2],3],
+    [9,[8,7]],
+    [[1,9],[8,5]],
 )
 
 for i in inputs:
-	print("----------")
-	preorder(i)
+    print("----------")
+    preorder(i)
 """
 
 #preorder([[[[[9,8],1],2],3],4])
-r = preorder([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])
+r, e = preorder([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])
 
-print(r)
+print(f"copy: {r}")
+print(f"exploder: {e}")
