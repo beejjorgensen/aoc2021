cf29185a5069bb1e2cfa57a9a20c3da8e6e97a07
Brian "Beej Jorgensen" Hall <beej@beej.us>
2021-12-20 12:22:33 -0800

    maybe working explosion

--- day18/solution1.py
+++ day18/solution1.py
@@ -1,39 +1,36 @@
 def preorder(n, depth=0, exploder=None):
+    # Leaf, check for right explosion
     if isinstance(n, int):
-        print(f"{depth}: leaf: {n}")
-        return n, exploder
-    
-    print(f">{depth}: {n}")
+        result = n
+
+        # Explode right
+        if exploder is not None:
+            result += exploder[1]
+            exploder[1] = 0  # Prevent subsequent adds
+
+        return result, exploder
 
     if depth == 4 and exploder is None:
         assert(isinstance(n[0], int) and isinstance(n[1], int))
         print(f"Explode {n}")
-        exploder = n
+        exploder = n.copy()
         return 0, exploder
 
     left, exploder = preorder(n[0], depth+1, exploder)
     right, exploder = preorder(n[1], depth+1, exploder)
 
-    print(f"<{depth}: {n}")
+    # Check for previous explosion to propagate left
+    if exploder is not None:
+        if isinstance(n[0], int):
+            # Explode left
+            left += exploder[0]
+            exploder[0] = 0  # Prevent subsequent adds
 
     return [left, right], exploder
 
 
-"""
-inputs = (
-    [1,2],
-    [[1,2],3],
-    [9,[8,7]],
-    [[1,9],[8,5]],
-)
+n = [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]
+r, _ = preorder(n)
 
-for i in inputs:
-    print("----------")
-    preorder(i)
-"""
-
-#preorder([[[[[9,8],1],2],3],4])
-r, e = preorder([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])
-
+print(f"orig: {n}")
 print(f"copy: {r}")
-print(f"exploder: {e}")
